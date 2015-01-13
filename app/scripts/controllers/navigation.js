44471c2f938cad0f26eb2354497af432f9645b25
Paul Cech <paul.cech92@gmail.com>
2015-01-13 13:50:21 +0100

    - fixed routing bug - added navigation controller

---
+++ app/scripts/controllers/navigation.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name applemarketApp.controller:NavigationCtrl
+ * @description
+ * # NavigationCtrl
+ * Controller of the applemarketApp
+ */
+angular.module('applemarketApp')
+  .controller('NavigationCtrl', function ($scope) {
+
+  });
