c23e18c60a1438d669a7559e7039ae535e0005dc
Paul Cech <paul.cech92@gmail.com>
2015-01-13 17:51:16 +0100

    - added angular translate module - added i18n for modals

---
+++ app/scripts/localization/en.js
@@ -0,0 +1,13 @@
+/**
+ * Created by paul on 04.01.15.
+ */
+
+'use strict';
+
+var app = angular.module('applemarketApp');
+
+app.config(function ($translateProvider) {
+  $translateProvider.translations('en', {
+    TITLE_TRADE: 'Negotiate '
+  });
+});
