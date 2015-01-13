f00195b41b8b57bebd7bc60440e924505e182d8d
Paul Cech <paul.cech92@gmail.com>
2015-01-13 17:59:05 +0100

    formatting fixes

--- app/scripts/localization/de.js
+++ app/scripts/localization/de.js
@@ -8,22 +8,22 @@ var app = angular.module('applemarketApp');
 
 app.config(function ($translateProvider) {
   $translateProvider.translations('de', {
-    TRADE_TITLE       : 'Verhandeln',
-    TRADE_INITIATED   : 'Das Angebot wird bearbeitet...',
-    TRADE_OFFER       : 'Angebot',
-    TRADE_PROFIT      : 'Profit',
-    TRADE_SEND_OFFER  : 'Angebot abgeben',
-    TRADE_ACCEPTED    : 'Angenommen',
-    TRADE_DECLINED    : 'Abgelehnt',
-    TRADE_OFFER_ACCEPTED : 'Angebot wurde angenommen.',
-    TRADE_OFFER_DECLINED : 'Angebot wurde abgelehnt.',
+    TRADE_TITLE           : 'Verhandeln',
+    TRADE_INITIATED       : 'Das Angebot wird bearbeitet...',
+    TRADE_OFFER           : 'Angebot',
+    TRADE_PROFIT          : 'Profit',
+    TRADE_SEND_OFFER      : 'Angebot abgeben',
+    TRADE_ACCEPTED        : 'Angenommen',
+    TRADE_DECLINED        : 'Abgelehnt',
+    TRADE_OFFER_ACCEPTED  : 'Angebot wurde angenommen.',
+    TRADE_OFFER_DECLINED  : 'Angebot wurde abgelehnt.',
 
-    WORD_WITH         : 'mit',
+    WORD_WITH             : 'mit',
 
-    SYSTEM_CANCEL     : 'Abbrechen',
-    SYSTEM_APPROVE    : 'Akzeptieren',
-    SYSTEM_ACCEPT     : 'Annehmen',
-    SYSTEM_DECLINE    : 'Ablehnen',
-    SYSTEM_OK         : 'Ok'
+    SYSTEM_CANCEL         : 'Abbrechen',
+    SYSTEM_APPROVE        : 'Akzeptieren',
+    SYSTEM_ACCEPT         : 'Annehmen',
+    SYSTEM_DECLINE        : 'Ablehnen',
+    SYSTEM_OK             : 'Ok'
   });
 });
